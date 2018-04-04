7483faeedb258548a3525a23da5d8a3cac652af4
tontheonelove <tonlove5233@gmail.com>
2018-04-04 16:54:20 +0700

    Initial commit

---
+++ app/src/main/java/ton/dev/gridlayout2018/ActivityTwo.java
@@ -0,0 +1,14 @@
+package ton.dev.gridlayout2018;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ActivityTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+
+    }
+}
